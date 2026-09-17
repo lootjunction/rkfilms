79a182249e228a8ca26df4385505d611c8c6aa73
lootjunction <attriji8@gmail.com>
2026-09-17 22:04:16 +0530

    app.py

--- app.py
+++ app.py
@@ -13,4 +13,5 @@ def service_worker():
     return "/* Service Worker Active */", 200, {'Content-Type': 'application/javascript'}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
